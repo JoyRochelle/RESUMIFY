b43ebf315c13da23ac4f5026fb92534382e40783
theofrolicdean <theofrolic@gmail.com>
2026-04-30 08:22:59 +0700

    chore: refactor blade template for user section

---
+++ resources/views/layouts/user/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html class="light" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <title>@yield('title', 'Resumify')</title>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&family=Manrope:wght@200..800&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-surface text-primary font-body antialiased @yield('body_class', 'min-h-screen flex')">
+    
+    @include('layouts.user.sidenavbar')
+
+    @yield('content')
+
+    @include('layouts.user.mobilenavbar')
+    
+</body>
+</html>
